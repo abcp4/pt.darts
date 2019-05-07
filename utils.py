729cd022b203fc9ae2a7fd4867b1878d741688e4
Ivan Pereira <navi1921@gmail.com>
2019-05-06 22:04:38 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -103,6 +103,9 @@ def accuracy(output, target, topk=(1,)):
     """ Computes the precision@k for the specified values of k """
     maxk = max(topk)
     batch_size = target.size(0)
+    print('output:',output)
+    print('target:',target)
+    print('maxk:',maxk)
 
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
