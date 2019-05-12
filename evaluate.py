c4ee3aa3f6258dec1814831f92078022a8dc7a3a
Ivan Pereira <navi1921@gmail.com>
2019-05-12 16:44:36 -0300

    Create evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,44 @@
+import os
+import torch
+import torch.nn as nn
+import numpy as np
+from tensorboardX import SummaryWriter
+from config import SearchConfig
+import utils
+from models.search_cnn import SearchCNNController
+from architect import Architect
+from visualize import plot
+
+
+config = SearchConfig()
+
+device = torch.device("cuda")
+
+# tensorboard
+writer = SummaryWriter(log_dir=os.path.join(config.path, "tb"))
+writer.add_text('config', config.as_markdown(), 0)
+
+logger = utils.get_logger(os.path.join(config.path, "{}.log".format(config.name)))
+config.print_params(logger.info)
+
+
+def main():
+    logger.info("Logger is set - training start")
+
+    # set default gpu device id
+    torch.cuda.set_device(config.gpus[0])
+
+    # set seed
+    np.random.seed(config.seed)
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed_all(config.seed)
+
+    torch.backends.cudnn.benchmark = True
+
+    # get data with meta info
+    input_size, input_channels, n_classes, train_data = utils.get_data(
+        config.dataset, config.data_path, cutout_length=0, validation=False)
+
+    net_crit = nn.CrossEntropyLoss().to(device)
+    model = SearchCNNController(input_channels, config.init_channels, n_classes, config.layers,
+net_crit, device_ids=config.gpus)
