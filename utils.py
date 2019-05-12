8a42d2ecf9b02cd1021752ae4afb7fc5d6355340
Ivan Pereira <navi1921@gmail.com>
2019-05-12 02:06:08 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -111,7 +111,7 @@ def accuracy(output, target, topk=(1,)):
     #print('target:',target)
     #print('maxk:',maxk)
     ###TOP 5 NAO EXISTE NAS MAAMAS OU NO GEO. TEM QUE TRATAR
-    maxk = 2 # Ignorando completamente o top5
+    maxk = 3 # Ignorando completamente o top5
 
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
