5451829eba92a0fce888fd886cf437b4daa45650
Ivan Pereira <navi1921@gmail.com>
2019-05-15 16:05:28 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -146,19 +146,21 @@ def save_checkpoint(model,epoch,w_optimizer,a_optimizer,loss, ckpt_dir, is_best=
         best_filename = os.path.join(ckpt_dir, 'best.pth.tar')
         shutil.copyfile(filename, best_filename)
         
-def load_checkpoint(model, optimizer, filename='checkpoint.pth.tar'):
+def load_checkpoint(model,epoch,w_optimizer,a_optimizer,loss, filename='checkpoint.pth.tar'):
 # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
     start_epoch = 0
     if os.path.isfile(filename):
         print("=> loading checkpoint '{}'".format(filename))
         checkpoint = torch.load(filename)
         print(checkpoint)
-        start_epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
+        epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['model_state_dict'])
+        w_optimizer.load_state_dict(checkpoint['w_optimizer_state_dict'])
+        a_optimizer.load_state_dict(checkpoint['a_optimizer_state_dict'])
+        loss = checkpoint['loss']
         print("=> loaded checkpoint '{}' (epoch {})"
                   .format(filename, checkpoint['epoch']))
     else:
         print("=> no checkpoint found at '{}'".format(filename))
 
-    return model, optimizer
+    return model,epoch,w_optimizer,a_optimizer,loss
