67769b1d3d4316155425ce3b55965606de602f22
Ivan Pereira <navi1921@gmail.com>
2019-06-05 00:32:38 -0300

    Update architect.py

--- architect.py
+++ architect.py
@@ -107,3 +107,64 @@ class Architect():
 
         hessian = [(p-n) / 2.*eps for p, n in zip(dalpha_pos, dalpha_neg)]
         return hessian
+    
+    def get_weights_from_arch(self, arch):
+        k = sum(1 for i in range(self.net._steps) for n in range(2+i))
+        num_ops = len(genotypes.PRIMITIVES)
+        n_nodes = self.net._steps
+
+        alphas_normal = Variable(torch.zeros(k, num_ops).cuda(), requires_grad=False)
+        alphas_reduce = Variable(torch.zeros(k, num_ops).cuda(), requires_grad=False)
+
+        offset = 0
+        for i in range(n_nodes):
+            normal1 = arch[0][2*i]
+            normal2 = arch[0][2*i+1]
+            reduce1 = arch[1][2*i]
+            reduce2 = arch[1][2*i+1]
+            alphas_normal[offset+normal1[0], normal1[1]] = 1
+            alphas_normal[offset+normal2[0], normal2[1]] = 1
+            alphas_reduce[offset+reduce1[0], reduce1[1]] = 1
+            alphas_reduce[offset+reduce2[0], reduce2[1]] = 1
+            offset += (i+2)
+
+        arch_parameters = [
+          alphas_normal,
+          alphas_reduce,
+        ]
+        return arch_parameters
+
+    def set_model_weights(self, weights):
+      self.net.alphas_normal = weights[0]
+      self.net.alphas_reduce = weights[1]
+      self.net._arch_parameters = [self.net.alphas_normal, self.net.alphas_reduce]
+
+    def sample_arch(self):
+        k = sum(1 for i in range(self.net._steps) for n in range(2+i))
+        num_ops = len(genotypes.PRIMITIVES)
+        n_nodes = self.net._steps
+
+        normal = []
+        reduction = []
+        for i in range(n_nodes):
+            ops = np.random.choice(range(num_ops), 4)
+            nodes_in_normal = np.random.choice(range(i+2), 2, replace=False)
+            nodes_in_reduce = np.random.choice(range(i+2), 2, replace=False)
+            normal.extend([(nodes_in_normal[0], ops[0]), (nodes_in_normal[1], ops[1])])
+            reduction.extend([(nodes_in_reduce[0], ops[2]), (nodes_in_reduce[1], ops[3])])
+
+        return (normal, reduction)
+
+
+    def perturb_arch(self, arch):
+        new_arch = copy.deepcopy(arch)
+        num_ops = len(genotypes.PRIMITIVES)
+
+        cell_ind = np.random.choice(2)
+        step_ind = np.random.choice(self.net._steps)
+        nodes_in = np.random.choice(step_ind+2, 2, replace=False)
+        ops = np.random.choice(range(num_ops), 2)
+
+        new_arch[cell_ind][2*step_ind] = (nodes_in[0], ops[0])
+        new_arch[cell_ind][2*step_ind+1] = (nodes_in[1], ops[1])
+        return new_arch
