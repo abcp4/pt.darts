e43b40aaafe3c81c92d74bf075e795fdfc39fdb5
Ivan Pereira <navi1921@gmail.com>
2019-06-07 20:05:19 -0300

    Update augment.py

--- augment.py
+++ augment.py
@@ -171,41 +171,88 @@ def train(train_loader, model, optimizer, criterion, epoch):
     logger.info("Train: [{:3d}/{}] Final Prec@1 {:.4%}".format(epoch+1, config.epochs, top1.avg))
 
 
-def validate(valid_loader, model, criterion, epoch, cur_step):
+def validate(valid_loader, model, epoch, cur_step,overall = False):
     top1 = utils.AverageMeter()
     top5 = utils.AverageMeter()
     losses = utils.AverageMeter()
-
     model.eval()
-
+    import numpy as np
+    preds = np.asarray([])
+    targets = np.asarray([])
     with torch.no_grad():
         for step, (X, y) in enumerate(valid_loader):
             X, y = X.to(device, non_blocking=True), y.to(device, non_blocking=True)
             N = X.size(0)
 
-            logits, _ = model(X)
-            loss = criterion(logits, y)
+            logits = model(X)
+            loss = model.criterion(logits, y)
 
             prec1, prec5 = utils.accuracy(logits, y, topk=(1, 5))
+            target = y
+            output = logits
+            topk = (1,3)
+            maxk = max(topk)
+            batch_size = target.size(0)
+            _, predicted = torch.max(output.data, 1)
+            #minha alteracao
+            preds = np.concatenate((preds,predicted.cpu().numpy().ravel()))
+            targets = np.concatenate((targets,target.cpu().numpy().ravel()))
+            
+            ###TOP 5 NAO EXISTE NAS MAAMAS OU NO GEO. TEM QUE TRATAR
+            maxk = 3 # Ignorando completamente o top5
+            
+            _, pred = output.topk(maxk, 1, True, True)
+            pred = pred.t()
+            
+            # one-hot case
+            if target.ndimension() > 1:
+                target = target.max(1)[1]
+
+            correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+            res = []
+            for k in topk:
+                correct_k = correct[:k].view(-1).float().sum(0)
+                res.append(correct_k.mul_(1.0 / batch_size))
+            prec1,prec5 = res
             losses.update(loss.item(), N)
             top1.update(prec1.item(), N)
             top5.update(prec5.item(), N)
 
+    
             if step % config.print_freq == 0 or step == len(valid_loader)-1:
                 logger.info(
-                    "Valid: [{:3d}/{}] Step {:03d}/{:03d} Loss {losses.avg:.3f} "
+                    "Valid: [{:2d}/{}] Step {:03d}/{:03d} Loss {losses.avg:.3f} "
                     "Prec@(1,5) ({top1.avg:.1%}, {top5.avg:.1%})".format(
                         epoch+1, config.epochs, step, len(valid_loader)-1, losses=losses,
                         top1=top1, top5=top5))
+            
+    print(preds.shape)
+    print(targets.shape)
+    print('np.unique(targets):',np.unique(targets))
+    print('np.unique(preds): ',np.unique(preds))
+    from sklearn.metrics import classification_report
+    from sklearn.metrics import accuracy_score
+    print(accuracy_score(targets, preds))
+    cr = classification_report(targets, preds,output_dict= True)
+    a1,a2,a3 = cr['macro avg']['f1-score'] ,cr['macro avg']['precision'],cr['macro avg']['recall'] 
+    topover = (a1+a2+a3)/3 
+    print(classification_report(targets, preds))
+    from sklearn.metrics import balanced_accuracy_score
+    from sklearn.metrics import accuracy_score
+    print(balanced_accuracy_score(targets, preds))
+    print(accuracy_score(targets, preds))
+    from sklearn.metrics import confusion_matrix
+    matrix = confusion_matrix(targets, preds)
+    print(matrix.diagonal()/matrix.sum(axis=1))
+    print(matrix)
 
-    writer.add_scalar('val/loss', losses.avg, cur_step)
-    writer.add_scalar('val/top1', top1.avg, cur_step)
-    writer.add_scalar('val/top5', top5.avg, cur_step)
-
-    logger.info("Valid: [{:3d}/{}] Final Prec@1 {:.4%}".format(epoch+1, config.epochs, top1.avg))
-
+    logger.info("Valid: [{:2d}/{}] Final Prec@1 {:.4%}".format(epoch+1, config.epochs, top1.avg))
+    logger.info("Valid: [{:2d}/{}] Overall {:.4%}".format(epoch+1, config.epochs, topover))
+    
+    if overall:
+        return topover
     return top1.avg
-
 
 if __name__ == "__main__":
     main()
