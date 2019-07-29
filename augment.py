829bb571bc7826b17e29ec5e4c7cbf387a2a0cc0
Ivan Pereira <navi1921@gmail.com>
2019-07-29 17:38:07 -0300

    Update augment.py

--- augment.py
+++ augment.py
@@ -38,7 +38,7 @@ def main():
     #input_size, input_channels, n_classes, train_data, valid_data = utils.get_data(
     #    config.dataset, config.data_path, config.cutout_length, validation=True)
     input_size, input_channels, n_classes, train_data, test_dat, val_dat = utils.get_data(
-        config.dataset, config.data_path, cutout_length=0, validation=True,validation2 = True)
+        config.dataset, config.data_path,config.val1_data_path,config.val2_data_path, cutout_length=0, validation=True,validation2 = True)
     print('input_size', input_size)
     criterion = nn.CrossEntropyLoss().to(device)
     use_aux = config.aux_weight > 0.
